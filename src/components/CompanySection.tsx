83417d11d8da2451e622bab86ba57b0c40c76388
Miguel-Codar <contato.msdev@gmail.com>
2025-07-04 13:22:05 -0300

    nova altecao

--- src/components/CompanySection.tsx
+++ src/components/CompanySection.tsx
@@ -6,12 +6,9 @@ const CompanySection = () => {
   const companyImages = [
     "/lovable-uploads/b1f0df19-504e-4b45-bc38-a2fd5def32c9.png",
     "/lovable-uploads/3fd96328-92ad-489a-ab9c-bc2ddcc9dac1.png",
-    "/lovable-uploads/db6909d7-b30c-474a-9bf7-b0fbb5b13c57.png",
     "/lovable-uploads/c419d0a9-4176-48ae-a591-3887eb8077b9.png",
-    "/lovable-uploads/7081af8d-e844-4f89-8aba-f99b4c7a0c71.png",
     "/lovable-uploads/c7524110-e322-42ab-9918-b0b4aa4b1aaf.png",
     "/lovable-uploads/4edf2385-3091-46ab-9c6b-be12f716a6c5.png",
-    "/lovable-uploads/931d18db-998c-4395-a6c4-3d0ff80568d2.png"
   ];
 
   return (
